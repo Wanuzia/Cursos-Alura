5a5904e39b7b307fed5caef2eb8ade9b133b17e5
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-08-30 22:43:01 -0300

    Refatora e cria módulo com package.json

--- js-programando-orientacao-objetos/index.js
+++ js-programando-orientacao-objetos/index.js
@@ -1,24 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-}
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-/*Adiciona comportamentos à classe com a criação de métodos.*/
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-        }
-    }
-
-    depositar(valor) {
-        if(valor >0){
-            this._saldo += valor;
-        }
-    }
-    
-}
+import {Cliente} from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
 
 const cliente1 = new Cliente();/*Cria instancia da classe*/
@@ -31,12 +12,12 @@ cliente2.cpf = 33344455566678;
 
 const contaCorrenteWanuzia = new ContaCorrente();
 contaCorrenteWanuzia._saldo = 0;
-contaCorrenteWanuzia.agencia = 001;
+contaCorrenteWanuzia.agencia = "001";
 
 contaCorrenteWanuzia.depositar(500);
-contaCorrenteWanuzia.sacar(50);
+const valorSacado = contaCorrenteWanuzia.sacar(50);
 
 console.log(cliente1);
 console.log(cliente2);
-console.log(contaCorrenteWanuzia.saldo)
+console.log(valorSacado, contaCorrenteWanuzia._saldo)
 
